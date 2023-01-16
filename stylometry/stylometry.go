2897ad0ea97943d43c272fe13404b79f0726a129
shvz0 <dmitrstan@gmail.com>
2023-01-16 15:22:28 +0600

    Refactor

--- stylometry/stylometry.go
+++ stylometry/stylometry.go
@@ -1,6 +1,7 @@
 package stylometry
 
 import (
+	"math"
 	"regexp"
 	"sort"
 	"strings"
@@ -111,4 +112,93 @@ func lengthDistribution(words []string) map[int]int {
 		distr[len(w)]++
 	}
 	return distr
+}
+
+func corpusFeatures(mostCommonWords []pair, wordFreqByAuthor map[string]map[string]float64) map[string]feature {
+	corpusFeatures := make(map[string]feature)
+
+	for _, v := range mostCommonWords {
+		corpusFeatures[v.word] = feature{}
+		avg := 0.0
+
+		for _, freqMap := range wordFreqByAuthor {
+			avg += freqMap[v.word]
+		}
+
+		if len(wordFreqByAuthor) != 0 {
+			avg /= float64(len(wordFreqByAuthor))
+			avg = roundFloat(avg, 5)
+		}
+
+		stdev := 0.0
+		for _, freqMap := range wordFreqByAuthor {
+			diff := freqMap[v.word] - avg
+			stdev += diff * diff
+			stdev = roundFloat(stdev, 5)
+		}
+
+		if len(wordFreqByAuthor)-1 != 0 {
+			stdev /= float64((len(wordFreqByAuthor) - 1))
+			stdev = roundFloat(math.Sqrt(stdev), 5)
+		}
+
+		corpusFeatures[v.word] = feature{Mean: avg, StdDev: stdev}
+	}
+
+	return corpusFeatures
+}
+
+func zScore(mostCommonWords []pair, corpusFeatures map[string]feature, freqMap map[string]float64) map[string]float64 {
+	cZscore := make(map[string]float64)
+
+	for _, v := range mostCommonWords {
+		val := freqMap[v.word]
+		mean := corpusFeatures[v.word].Mean
+		stdDev := corpusFeatures[v.word].StdDev
+		if stdDev != 0 {
+			cZscore[v.word] = roundFloat((val-mean)/stdDev, 5)
+		}
+	}
+
+	return cZscore
+}
+
+func aggregateCorporaByAuthors(corpora []*Corpus) map[string]*Corpus {
+	wordsByAuthor := make(map[string][]string)
+
+	for _, c := range corpora {
+		wordsByAuthor[c.Author] = append(wordsByAuthor[c.Author], c.Corpus...)
+	}
+
+	corpusByAuthor := make(map[string]*Corpus)
+
+	for a, w := range wordsByAuthor {
+		c := Corpus{Author: a, Corpus: w}
+		c.freq()
+		if _, ok := corpusByAuthor[a]; !ok {
+			corpusByAuthor[a] = &c
+		} else {
+			corpusByAuthor[a].Corpus = append(corpusByAuthor[a].Corpus, c.Corpus...)
+		}
+	}
+
+	return corpusByAuthor
+}
+
+func combineCorporaToCorpus(corpora []*Corpus) []string {
+	var corpus []string
+	for _, v := range corpora {
+		corpus = append(corpus, v.Corpus...)
+	}
+	return corpus
+}
+
+func roundFloat(x float64, decimalPlaces int) float64 {
+	if decimalPlaces == 0 {
+		return math.Round(x)
+	}
+
+	p := float64(math.Pow10(decimalPlaces))
+
+	return math.Round(x*p) / p
 }
